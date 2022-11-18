f237ec1073d19ebf2d7f781c472f77d64e3a6169
Jarne <jarne3011@hotmail.com>
2022-11-18 13:07:30 +0100

    Addplayer

--- Project_Webapplicaties/Controllers/AdminController.cs
+++ Project_Webapplicaties/Controllers/AdminController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Project_Webapplicaties.Data.Repository.Interfaces;
 
 namespace Project_Webapplicaties.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly IPlayerRepository _playerRepository;
+
+        public AdminController(IPlayerRepository playerRepository)
+        {
+            _playerRepository = playerRepository;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult AddPlayer()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
